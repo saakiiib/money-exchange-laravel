065ad5ea1c117a8f304e934903ce11898a7fbf56
Sakib <nsakib.miraz@gmail.com>
2024-01-08 19:19:40 +0600

    Role, id added

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,15 +12,19 @@
                     <table class="table table-bordered">
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>Name</th>
                                 <th>Email</th>
+                                <th>Role</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($users as $user)
                             <tr>
+                                <td>{{ $user->id }}</td>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
+                                <td>{{ $user->role }}</td>
                             </tr>
                             @endforeach
                         </tbody>
